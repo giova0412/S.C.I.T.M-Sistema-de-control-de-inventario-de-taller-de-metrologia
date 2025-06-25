4ae28547bdc09221143f4eaf1743f44f23a163f8
giova0412 <230314@utxicotepec.edu.mx>
2025-06-25 16:34:27 -0600

    se estan agregando productos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 //conexion ala base de datos
 import mongoose,{mongo} from "mongoose";
-mongoose.connect('mongodb+srv://230314Giovany:Fabuloso1*3@cluster0g.panx4.mongodb.net/groceries_db?retryWrites=true&w=majority&appName=Cluster0G')
+mongoose.connect('mongodb+srv://pemexmetro580:metrologia1*3@clustermetro.jpdlrc4.mongodb.net/TallerMetro?retryWrites=true&w=majority&appName=ClusterMetro')
 .then((db)=>console.log('conexion a mongo lista 👍'))
 .catch((error)=>console.error(error));
 export default mongoose;
